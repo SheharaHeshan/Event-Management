fdc76b4ab1669f11aa8da3255c1f7395cc4d3d8c
@SheharaHeshan <sheharaheshan@gmail.com>
2025-09-13 07:53:52 +0530

    init commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.mfx.eventmanagement {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.mfx.eventmanagement to javafx.fxml;
+    exports com.mfx.eventmanagement;
+}
